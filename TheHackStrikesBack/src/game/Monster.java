a5c43680106d51b7d94d80f7645c8fcdb0a5f124
ishan <ishan@LAPTOP-67TG7MSC>
2023-04-01 14:41:41 -0400

    monster push

--- TheHackStrikesBack/src/game/Monster.java
+++ TheHackStrikesBack/src/game/Monster.java
@@ -2,10 +2,25 @@ package game;
 
 public class Monster extends Creature {
 	private Entity target;
+	private boolean targeting;
 	
-	public void setTarget(Entity target) {
+	public Monster(float x, float y) {
+		super(x);
+		super(y);
+		targeting = false;
+	}
+	
+	public void setTarget(Creature target) {
 		this.target = target;
+		targeting = true;
+	}
+	
+	public boolean isTargeting() {
+		return targeting;
 	}
 	
 }
-gfg
+
+
+
+
