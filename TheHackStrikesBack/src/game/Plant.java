49d8d8311126ecd38298b05eff24b4dc8e6b5389
Akshat752 <akshatprakash8@gmail.com>
2023-04-01 16:36:51 -0400

    plant stats

--- TheHackStrikesBack/src/game/Plant.java
+++ TheHackStrikesBack/src/game/Plant.java
@@ -5,17 +5,16 @@ import processing.core.PImage;
 
 public class Plant extends Entity {
 
-	private int radius, speed, startX, startY, level;
+	private int startX, startY, level,radius,speed;
 	private PImage image;
 	private World world;
 	private PApplet parent;
 
-	public Plant(PApplet parent, float x, float y, int health, int radiusOfGrowth, int growthSpeed, World world) {
+	public Plant(PApplet parent, float x, float y, int health, World world) {
 		super(parent, x, y, health);
 		this.parent = parent;
 		upgrade();
-		radius = radiusOfGrowth;
-		speed = growthSpeed;
+
 		this.world = world;
 		startX = Math.round(x) - radius;
 		startY = Math.round(y) - radius;
@@ -55,6 +54,21 @@ public class Plant extends Entity {
 	public void upgrade() {
 		if(level < 3)
 			level++;
+		
+		if(level == 1) {
+			radius = 3;
+			speed = 3;
+		}
+		else if(level == 2) {
+			radius = 5;
+			speed = 5;
+			
+		}
+		else if(level == 3) {
+			radius = 9;
+			speed = 9;
+			
+		}
 		
 		image = parent.loadImage("art/plant" + level + ".png");
 		
