a0fe16378e4ec25a826ba8675ddbf22951da8355
trevorsmith <trevorsmith@localhost>
2023-04-01 12:53:42 -0400

    drawer

---
+++ TheHackStrikesBack/src/game/WorldDrawer.java
@@ -0,0 +1,28 @@
+package game;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class WorldDrawer 
+{
+	private PApplet p;
+	private World world;
+	private int tileSize;
+	// Width of the screen in tiles
+	private int width;
+	// Height of the screen in tiles
+	private int height;
+	
+	private PImage ground;
+	private PImage deadGround;
+	private PImage water;
+	private PImage deadWater;
+	
+	public WorldDrawer(PApplet parent, World world, int tileSize, int width, int height)
+	{
+		p = parent;
+		this.world = world;
+		
+		
+	}
+}
