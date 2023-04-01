0d7396cfbea6f0f26ad742e598c879242b9efcfc
caitlynhart <caitlynhart@localhost>
2023-04-01 16:22:37 -0400

    adsf

--- TheHackStrikesBack/src/game/Plant.java
+++ TheHackStrikesBack/src/game/Plant.java
@@ -4,7 +4,7 @@ import processing.core.PApplet;
 
 public class Plant extends Entity {
 
-	private int radius, speed;
+	private int radius, speed, startX, startY;
 	private World world;
 
 	public Plant(PApplet parent, float x, float y, int health, int radiusOfGrowth, int growthSpeed, World world) {
@@ -12,6 +12,8 @@ public class Plant extends Entity {
 		radius = radiusOfGrowth;
 		speed = growthSpeed;
 		this.world = world;
+		startX = Math.round(x) - radius;
+		startY = Math.round(y) - radius;
 	}
 
 	public void behavior() {
@@ -26,19 +28,30 @@ public class Plant extends Entity {
 	private void cleanse() {
 		boolean cleanedTile = false;
 		while(!cleanedTile) {
-			
+			int randomX = (int) (Math.random()*(radius * 2 + 1))+startX;
+			int randomY = (int) (Math.random()*(radius * 2 + 1))+startY;
+			if(world.getMap()[randomX][randomY] < 0)
+				cleanedTile = true;
+				world.getMap()[randomX][randomY] *= -1;
 		}
 	}
 
 	private boolean checkArea() {
-		int startX = Math.round(x) - radius;
-		int startY = Math.round(y) - radius;
-		for (int r = startY; r <= startY + (radius * 2) + 1; r++) {
-			for (int c = startX; c <= startX + (radius * 2) + 1; c++) {
+		
+		for (int r = startY; r < startY + (radius * 2 + 1); r++) {
+			for (int c = startX; c < startX + (radius * 2 + 1); c++) {
 				if (world.getTile(r, c) == -1)
 					return false;
 			}
 		}
 		return true;
 	}
+
+	@Override
+	public void draw(float imageX, float imageY) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
