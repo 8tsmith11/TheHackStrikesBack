d773076a4d5bf69305823cec34d432d65e56b454
trevorsmith <trevorsmith@localhost>
2023-04-01 12:41:32 -0400

    main method

---
+++ TheHackStrikesBack/src/game/Game.java
@@ -0,0 +1,17 @@
+package game;
+
+import processing.core.PApplet;
+
+public class Game extends PApplet
+{
+
+	public static void main(String[] args) 
+	{
+		PApplet.main("game.Game");
+	}
+	
+	public void settings()
+	{
+		
+	}
+}
--- TheHackStrikesBack/src/game/World.java
+++ TheHackStrikesBack/src/game/World.java
@@ -1,6 +1,23 @@
 package game;
 
+import java.util.ArrayList;
+
 public class World 
 {
 	private int[][] map;
+	private ArrayList<Entity> entities;
+	
+	public World(int size)
+	{
+		map = new int[size][size];
+		for (int r = 0; r < map.length; r++)
+		{
+			for (int c = 0; c < map.length; c++)
+			{
+				map[r][c] = -1;
+			}
+		}
+		
+		entities = new ArrayList<Entity>();
+	}
 }
