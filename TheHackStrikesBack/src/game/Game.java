2ccbfe2228bf4cd1428abdfa0a4f44d42373053f
trevorsmith <trevorsmith@localhost>
2023-04-01 16:37:05 -0400

    work

--- TheHackStrikesBack/src/game/Game.java
+++ TheHackStrikesBack/src/game/Game.java
@@ -47,7 +47,7 @@ public class Game extends PApplet
 			if(keyCode == DOWN) {
 				world.getRobot().moveY(1);
 			}
-			//world.getRobot().setMoving(false);
+			//world.getRobot().setMoving(false); s
 			
 		}
 	}
