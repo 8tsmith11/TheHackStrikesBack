642cb07163393ae304fb8b65871654940a313cbd
caitlynhart <caitlynhart@localhost>
2023-04-01 14:41:41 -0400

    psuh

---
+++ TheHackStrikesBack/src/game/Plant.java
@@ -0,0 +1,23 @@
+package game;
+
+public class Plant extends Entity{
+	
+	
+	public Plant(int radiusOfGrowth, int growthSpeed) {
+		
+	}
+	
+	private void behavior()
+	{
+		double success = Math.random();
+		if (success < .001 * growthSpeed)
+		{
+			cleanse();
+		}
+	}
+	
+	private void cleanse()
+	{
+		for (int )
+	}
+}
