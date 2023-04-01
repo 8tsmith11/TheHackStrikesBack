943ee6d8b87eef62932dfb05eba33d5e502005be
trevorsmith <trevorsmith@localhost>
2023-04-01 16:48:22 -0400

    changed movement

--- TheHackStrikesBack/src/game/Game.java
+++ TheHackStrikesBack/src/game/Game.java
@@ -8,6 +8,11 @@ public class Game extends PApplet
 	private World world;
 	private WorldDrawer wd;
 	private int tileSize = 32;
+	
+	private int leftInput;
+	private int rightInput;
+	private int upInput;
+	private int downInput;
 
 	public static void main(String[] args) 
 	{
@@ -27,40 +32,54 @@ public class Game extends PApplet
 	public void draw()
 	{
 		wd.drawWorld();
+		world.getRobot().moveX(leftInput + rightInput);
+		world.getRobot().moveY(downInput + upInput);
 	}
 	
-	
-	public void keyPressed() {
-		if(key == CODED) {
+	public void keyPressed()
+	{
+		if (key == 'w' || keyCode == UP)
+		{
 			world.getRobot().setMoving(true);
-			if (keyCode == RIGHT) {
-				world.getRobot().moveX(1);
-			}
-			
-			if(keyCode == LEFT) {
-				world.getRobot().moveX(-1);
-			}
-			//afsjkjkfadjkfaddfskajdfaskjafdjksafkdskfdsajjk
-			if(keyCode == UP) {
-				world.getRobot().moveY(-1);
-			}
-			if(keyCode == DOWN) {
-				world.getRobot().moveY(1);
-			}
-			//world.getRobot().setMoving(false); s
-			
+			upInput = -1;
+		}
+		else if (key == 'a' || keyCode == LEFT)
+		{
+			world.getRobot().setMoving(true);
+			leftInput = -1;
+		}
+		else if (key == 's' || keyCode == DOWN)
+		{
+			world.getRobot().setMoving(true);
+			downInput = 1;
+		}
+		else if (key == 'd' || keyCode == RIGHT)
+		{
+			world.getRobot().setMoving(true);
+			rightInput = 1;
 		}
 	}
 	
-	public void keyReleased() {
+	public void keyReleased()
+	{
 		if(key == CODED) {
 			world.getRobot().setMoving(false);
-		
-	
-			//world.getRobot().setMoving(false);
 			
+		if (key == 'w' || keyCode == UP)
+		{
+			upInput = 0;
+		}
+		else if (key == 'a' || keyCode == LEFT)
+		{
+			leftInput = 0;
+		}
+		else if (key == 's' || keyCode == DOWN)
+		{
+			downInput = 0;
+		}
+		else if (key == 'd' || keyCode == RIGHT)
+		{
+			rightInput = 0;
 		}
 	}
-	
-	
 }
