b8c41aed300c951adc9f18800453dfef00c25bb4
ishan <ishan@LAPTOP-67TG7MSC>
2023-04-01 17:39:59 -0400

    lakesize

--- TheHackStrikesBack/src/game/World.java
+++ TheHackStrikesBack/src/game/World.java
@@ -19,7 +19,7 @@ public class World
 			for (int c = 0; c < map.length; c++)
 			{
 				map[r][c] = -1;
-				if(Math.random() < 0.01) {
+				if(Math.random() < 0.001) {
 					map[r][c] = -2;
 				}
 				else {
