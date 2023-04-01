f819261dbd50b80ed80db6189915cd5a7a81b252
caitlynhart <caitlynhart@localhost>
2023-04-01 17:13:17 -0400

    fff

--- TheHackStrikesBack/src/game/Laser.java
+++ TheHackStrikesBack/src/game/Laser.java
@@ -1,16 +1,25 @@
 package game;
 
+import processing.core.PApplet;
+import processing.core.PImage;
+
 public class Laser extends Entity{
 	
 	private float toX, toY;
 	
-	public Laser(PApplet parent, float toX, float toY) {
-		super(parent, )
-		this.toX = toX;
-		this.toY = toY;
+	private PImage laserImage;
+	private PApplet parent;
+	
+	public Laser(PApplet parent, float x, float y) {
+		super(parent, x, y, 100000);
+		toX = parent.mouseX;
+		toY = parent.mouseY;
+		laserImage = new PImage();
 	}
 	
-	
+	public void draw() {
+		parent.image()
+	}
 	
 	
 }
